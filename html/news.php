514a630cd70bd7b1d4199faa02a20b2755df36ca
Joe Leigh <jleigh@illinois.edu>
2016-08-23 16:22:27 -0500

    Made news page more user friendly for admins

--- html/news.php
+++ html/news.php
@@ -1,72 +1,20 @@
 <?php
 require_once 'includes/header.inc.php';
 
-$adminType = 1;
-$selectedArticle = 0;
-
 $article = new Articles($sqlDataBase);
 $user = new User($sqlDataBase);
 
-if (isset($_GET['edit'])) {
-	$selectedArticle = $_GET['edit'];
-	$article->LoadArticle($selectedArticle);
-}
-
-if (isset($_GET['delete']) && $login_user->isAdmin()) {
-	$article->RemoveArticle($_GET['delete']);
-}
-
-
-if (isset($_POST['applyEdit']) && $login_user->isAdmin()) {
-	$title = $_POST['title'];
-	$bodyText = $_POST['text'];
-	$articleId = $_POST['editArticleId'];
-	$article->LoadArticle($articleId);
-	$article->setTitle($title);
-	$article->setDescription($bodyText);
-	$article->setUserid($authenticate->getAuthenticatedUser()->GetUserId());
-	$article->UpdateArticle();
-	$article = new Articles($sqlDataBase);
-}
-
-if (isset($_POST['createNew']) && $login_user->isAdmin()) {
-
-	$title = $_POST['title'];
-	$bodyText = $_POST['text'];
-	$article->CreateArticle($authenticate->getAuthenticatedUser()->GetUserId(), $title, $bodyText);
-}
 $articlesList = $article->GetArticles();
 ?>
 
-
+<div>
+	<?php if($login_user->isAdmin()){ ?>
+	<a href="edit_news.php" class="btn btn-xs btn-primary pull-right"><span class="glyphicon glyphicon-plus"></span> Add News</a>
+	<?php } ?>
 	<h3>Latest News</h3>
+</div>
 
 <?php
-// Admin controls
-if ($login_user->isAdmin()) {
-?>
-<form name="articlesForms" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<div class="well">
-		<div class="form-group">
-			<label for="newsTitle">Title:</label><br>
-			<input type="text" size="60" value="<?php echo $article->getTitle();?>" name="title" class="form-control">
-		</div>
-		<div class="form-group">
-			<label for="newsContent">Content:</label>
-			<textarea name="text" style="width:100%; height:100px;" class="form-control"><?php echo $article->getDescription();?></textarea>
-		</div>
-			<input type="hidden" name="editArticleId" value="<?php echo $article->getArticleId();?>">
-<?php
-		if($article->getArticleId()) {
-			echo "<input type=\"submit\" class=\"btn btn-primary\" name=\"applyEdit\" value=\"Edit Article\">";
-		} else {
-			echo "<input type=\"submit\" class=\"btn btn-primary\" name=\"createNew\" value=\"Create New Article\">";
-		}
-?>
-	</div>
-</form>
-<?php
-}
 
 foreach ($articlesList as $id => $articleInfo) { 
 	$formattedText = str_replace("\n", "<br/>", $articleInfo['text']);
@@ -79,7 +27,7 @@ foreach ($articlesList as $id => $articleInfo) {
 	<div class="panel-footer">
 		<?php
 		if ($login_user->isAdmin()) {
-			echo "<a href=\"news.php?edit=" . $articleInfo['id'] . "\">Edit</a> | <a href=\"news.php?delete=" . $articleInfo['id'] . "\">Delete</a> | ";
+			echo "<a href=\"edit_news.php?edit=" . $articleInfo['id'] . "\">Edit</a> | <a href=\"edit_news.php?delete=" . $articleInfo['id'] . "\">Delete</a> | ";
 		}
 		$user->LoadUser($articleInfo['user_id']);
 		echo "<small>".$user->GetFirst() . " " . $user->GetLast() . " | " . $user->GetEmail() . " | " . $articleInfo['created'] . "</small>";
