43cd3cca62b10f5b7c11e845e6e8c2fbbf006852
Joe Leigh <jleigh@illinois.edu>
2017-06-01 10:10:59 -0500

    Added script to dump instrument permissions to ldap

---
+++ bin/ldapdump.php
@@ -0,0 +1,35 @@
+<?php
+// setting up the web root and server root for
+include('../html/includes/config.php');
+include('../html/includes/auto_load_classes.php');
+include('../html/includes/mysql_connect.php');
+
+//Sets up ldap connection
+if(LDAPMAN_API_ENABLED){
+	$ldapman = new LdapManager(LDAPMAN_API_URL, LDAPMAN_API_USERNAME, LDAPMAN_API_PASSWORD);
+} else {
+	$ldapman = new LdapManager(LDAPMAN_API_URL);
+}
+
+$selectedUser = new User($sqlDataBase);
+$allUsers = $selectedUser->GetAllUsers();
+$dev = new Device($sqlDataBase);
+$allDevices = $dev->GetDevicesList();
+$accessControl = new AccessControl($sqlDataBase);
+
+foreach($allDevices as $device){
+	if($device['status_id']==1){ // Only devices with tracking enabled
+		echo "Processing ".$device['device_name']."\n";
+		foreach($allUsers as $user){
+			$accessExists = $accessControl->AccessExists(AccessControl::RESOURCE_DEVICE, $device['id'], AccessControl::PARTICIPANT_USER, $user['id']);
+			if($accessExists !== 0 && $accessExists['permission'] != 0){
+				echo "\tAdding ".$user['user_name']." to ".LDAPMAN_GROUP_PREFIX.$device['device_name']."... ";
+				if($ldapman->addGroupMember(LDAPMAN_GROUP_PREFIX.$device['device_name'],$user['user_name'])){
+					echo "\n";
+				} else {
+					echo $user['user_name']." does not exist.\n";
+				}
+			}
+		}
+	}
+}
