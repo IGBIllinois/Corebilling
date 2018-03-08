323e86b8ea151dd3c39552b8f687391517ba8e54
Joe Leigh <jleigh@illinois.edu>
2018-03-08 10:28:29 -0600

    Added a script to display all users who do not have valid LDAP accounts

---
+++ bin/removedusers.php
@@ -0,0 +1,21 @@
+<?php
+// setting up the web root and server root for
+include('../html/includes/config.php');
+include('../html/includes/auto_load_classes.php');
+include('../html/includes/mysql_connect.php');
+
+//Sets up ldap connection
+if(LDAPMAN_API_ENABLED){
+	$ldapman = new LdapManager(LDAPMAN_API_URL, LDAPMAN_API_USERNAME, LDAPMAN_API_PASSWORD);
+} else {
+	$ldapman = new LdapManager(LDAPMAN_API_URL);
+}
+
+$selectedUser = new User($sqlDataBase);
+$allUsers = $selectedUser->GetUsers(5);
+
+foreach($allUsers as $user){
+	if($ldapman->getUser($user['user_name']) == null){
+		echo $user['user_name']."\n";
+	}
+}
