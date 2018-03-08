b89a7f68910a8ca3f1812f565210838fa2addd4d
Joe Leigh <jleigh@illinois.edu>
2018-03-08 10:25:59 -0600

    Group and Device dropdowns on Facility Billing only show exact matches (fixed)

--- html/js/main.inc.js
+++ html/js/main.inc.js
@@ -1,12 +1,20 @@
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
 function searchCol(table,column,field){
 	var $filter = jQuery('#'+field);
-	table.column(column).search($filter.val());
+	table.column(column).search(escapeRegExp($filter.val()));
 	table.draw();
 }
 
 function matchCol(table,column,field){
 	var $filter = jQuery('#'+field);
-	table.column(column).search('^'+$filter.val()+'$',true,false);
+	if($filter.val() == ""){
+		table.column(column).search("");
+	} else {
+		table.column(column).search('^'+escapeRegExp($filter.val())+'$',true,false);
+	}
 	table.draw();
 }
 
