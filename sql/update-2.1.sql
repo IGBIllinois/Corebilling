115bef15ca4d095473dda3f70dd769edc1b02fd5
David Slater <dslater@illinois.edu>
2021-08-29 11:54:58 -0500

    reorganzied sql folder

--- sql/update-2.1.sql
+++ sql/update-2.1.sql
@@ -1,4 +1,17 @@
--- Run these sql commands to refactor the access_control table
+# Run this file to update the database for the core server update
+alter table `groups`
+    add column netid varchar(255);
+
+create table `user_groups`
+(
+    `user_id`  int(10) unsigned not null,
+    `group_id` int(10) unsigned not null,
+    primary key (`user_id`, `group_id`)
+) engine = InnoDB
+  default charset = latin1;
+insert into user_groups (user_id, group_id) select id as user_id, group_id from users;
+
+alter table `users` drop column group_id;-- Run these sql commands to refactor the access_control table
 delete from access_control where resource_type_id=2;
 delete from access_control where participant_type_id=0;
 alter table access_control drop column resource_type_id;
