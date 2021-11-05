87b4e9ad571e811d20f9691104d0735807190f5a
David Slater <dslater@illinois.edu>
2021-11-05 17:55:49 -0500

    Added settings to about page

--- html/about.php
+++ html/about.php
@@ -28,7 +28,53 @@ echo $extensions_string;
 </table>
 </div>
 </div>
-
+<div class='row'>
+<h3>Settings</h3>
+<div class='col-md-8 col-lg-8 col-xl-8'>
+<table class='table table-bordered table-sm'>
+<tr><td>LDAP_HOST</td><td><?php echo LDAP_HOST; ?></td></tr>
+<tr><td>LDAP_PEOPLE_DN</td><td><?php echo LDAP_PEOPLE_DN; ?></td></tr>
+<tr><td>LDAP_GROUP_DN</td><td><?php echo LDAP_GROUP_DN; ?></td></tr>
+<tr><td>LDAP_PORT</td><td><?php echo LDAP_PORT; ?></td></tr>
+<tr><td>TIMEZONE</td><td><?php echo settings::get_timezone(); ?></td></tr>
+<tr><td>LDAPMAN_API_ENABLED</td><td><?php echo LDAPMAN_API_ENABLED; ?></td></tr>
+<tr><td>LDAPMAN_API_URL</td><td><?php echo LDAPMAN_API_URL; ?></td></tr>
+<tr><td>LDAPMAN_DEVICE_PREFIX</td><td><?php echo LDAPMAN_DEVICE_PREFIX; ?></td></tr>
+<tr><td>LDAPMAN_PI_PREFIX'</td><td><?php echo LDAPMAN_PI_PREFIX; ?></td></tr>
+<tr><td>DATASERVER_ENABLED</td><td><?php echo settings::get_dataserver_enabled(); ?></td></tr>
+<tr><td>DATASERVER_ROOT_DIR</td><td><?php echo settings::get_dataserver_root_dir(); ?></td></tr>
+<tr><td>DB_HOST</td><td><?php echo DB_HOST; ?></td></tr>
+<tr><td>DB_NAME</td><td><?php echo DB_NAME; ?></td></tr>
+<tr><td>TITLE</td><td><?php echo settings::get_title(); ?></td></tr>
+<tr><td>LOGIN_TIMEOUT (seconds)</td><td><?php echo LOGIN_TIMEOUT; ?></td></tr>
+<tr><td>CAL_DEFAULT_COLOR</td>
+	<td><?php echo CAL_DEFAULT_COLOR; ?>&nbsp
+	<svg width="20" height="20"><rect width="20" height="20" style="fill:<?php echo CAL_DEFAULT_COLOR; ?>;stroke-width:3;stroke:rgb(0,0,0)" /></svg>
+	</td>
+</tr>
+<tr><td>CAL_TRAINING_COLOR</td>
+	<td><?php echo CAL_TRAINING_COLOR; ?>&nbsp
+	<svg width="20" height="20"><rect width="20" height="20" style="fill:<?php echo CAL_TRAINING_COLOR; ?>;stroke-width:3;stroke:rgb(0,0,0)" /></svg>
+	</td>
+</tr>
+<tr><td>CAL_MISSED_COLOR</td>
+	<td><?php echo CAL_MISSED_COLOR; ?>&nbsp
+	<svg width="20" height="20"><rect width="20" height="20" style="fill:<?php echo CAL_MISSED_COLOR; ?>;stroke-width:3;stroke:rgb(0,0,0)" /></svg>
+        </td>
+</tr>
+<tr><td>CAL_ROOM_COLOR</td>
+	<td><?php echo CAL_ROOM_COLOR; ?>&nbsp
+	<svg width="20" height="20"><rect width="20" height="20" style="fill:<?php echo CAL_ROOM_COLOR; ?>;stroke-width:3;stroke:rgb(0,0,0)" /></svg>
+        </td>
+</tr>
+<tr><td>RESERVE_ROOM</td><td><?php echo RESERVE_ROOM; ?></td></tr>
+<tr><td>USER_EXCEPTIONS_ARRAY</td><td><?php echo implode(',',USER_EXCEPTIONS_ARRAY); ?></td></tr>
+<tr><td>ENABLE_LOG</td><td><?php settings::get_log_enabled(); ?></td></tr>
+<tr><td>PASSWORD_RESET_URL</td><td><?php settings::get_password_reset_url(); ?></td></tr>
+<tr><td>NEWS_AGE (days)</td><td><?php echo settings::get_news_age(); ?></td></tr>
+</table>
+</div>
+</div>
 <?php
 require_once 'includes/footer.inc.php';
 
--- html/session.php
+++ html/session.php
@@ -1,32 +1,32 @@
 <?php
 
-//Check if the proper get inputs are set
+//Check if the proper $_POST inputs are set
 if (isset($_POST['username']) && $_POST['username']!="" && isset($_POST['key'])) {
-    require_once('includes/main.inc.php');
-    $deviceInfo = new Device($db);
-    $deviceInfo->load(0, $_POST['key']);
-    //check if device token matches
+	require_once('includes/main.inc.php');
+	$deviceInfo = new Device($db);
+	$deviceInfo->load(0, $_POST['key']);
 	$ipaddress = $_SERVER['REMOTE_ADDR'];
 
-    if ($deviceInfo->getId() > 0) {
-        $userId = User::exists($db,$_POST['username']);
+	if ($deviceInfo->getId() > 0) {
+		$userId = User::exists($db,$_POST['username']);
 
-        //check if user_name exists
-        if ($userId) {
-            //Start tracking session
-            Session::trackSession($db,$deviceInfo->getId(), $userId,$ipaddress);
-        } else {
-            //User was not found in website database so check for user exceptions
-		if (in_array(strtolower($_POST['username']), array_map('strtolower', $USER_EXCEPTIONS_ARRAY))){
-			$deviceInfo->updateLastTick();
-		}
+		//check if user_name exists
+		if ($userId) {
+			//Start tracking session
+			Session::trackSession($db,$deviceInfo->getId(), $userId,$ipaddress);
+		} 
 		else {
-			$deviceInfo->updateLastTick($_POST['username']);
-	        }
+			//User was not found in website database so check for user exceptions
+			if (in_array(strtolower($_POST['username']), array_map('strtolower',USER_EXCEPTIONS_ARRAY))) {
+				$deviceInfo->updateLastTick();
+			}
+			else {
+				$deviceInfo->updateLastTick($_POST['username']);
+			}
 	            
-        }
+		}
 
-    }
+	}
 }
 
 ?>	
