20cf9598faafb719162d82ed59f70df90ad8f507
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2022-03-13 14:40:01 -0500

    Fixed merge issues

--- conf/app.inc.php
+++ conf/app.inc.php
@@ -3,11 +3,7 @@
 Stores application variables
 */
 
-<<<<<<< HEAD
 define("VERSION","2.4");
-=======
-define("VERSION","2.3");
->>>>>>> 09d84842c14dba7dbe3e15b71cc03e75f68b5312
 define("CODEWEBSITE_URL","https://github.com/IGBIllinois/Corebilling");
 define("COREBILLINGSERVICE_URL","https://github.com/IGBIllinois/CoreBillingService/releases");
 define("LOG_DIR","log");
